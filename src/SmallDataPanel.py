910decc69ba28407e375cdddcfa752e81f6e5eee
yoon82 <yoon82@SLAC.STANFORD.EDU>
2016-10-03 20:42:10 +0000

    added blue to smalldata

--- src/SmallDataPanel.py
+++ src/SmallDataPanel.py
@@ -123,7 +123,10 @@ class SmallData(object):
 
     def updateQuantifierPlot(self, metric):
         self.w9.getPlotItem().clear()
-        self.curve = self.w9.plot(metric, pen=(200, 200, 200), symbolBrush=(255, 0, 0), symbolPen='w')
+        if len(np.where(metric==-1)[0]) > 0:
+            self.curve = self.w9.plot(metric, pen=(200, 200, 200), symbolBrush=(255, 0, 0), symbolPen='k') # red
+        else: # Every event was processed
+            self.curve = self.w9.plot(metric, pen=(200, 200, 200), symbolBrush=(0, 0, 255), symbolPen='k') # blue
         self.w9.setLabel('left', "Small data")
         if self.quantifier_sort:
             self.w9.setLabel('bottom', "Sorted Event Index")
