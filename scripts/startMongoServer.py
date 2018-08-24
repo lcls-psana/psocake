65213737dfab48b76d22eeed3804cba2df72c8d7
Tate Keller <30704183+tatkeller@users.noreply.github.com>
2018-08-24 09:19:29 -0700

    typo fix

--- scripts/startMongoServer.py
+++ scripts/startMongoServer.py
@@ -2,7 +2,7 @@ import subprocess
 import socket
 
 #Start Database
-subprocess.call(mongod --dbpath /reg/d/psdm/cxi/cxitut13/res/autosfx/db1 --bind_ip_all)
+subprocess.call("mongod --dbpath /reg/d/psdm/cxi/cxitut13/res/autosfx/db1 --bind_ip_all", shell = True)
 
 #Locate GPU Host
 host = socket.gethostname()
