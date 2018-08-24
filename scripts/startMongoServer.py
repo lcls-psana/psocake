acc2d3bc8a395245ef65d42bbea69df0788824a9
Tate Keller <30704183+tatkeller@users.noreply.github.com>
2018-08-24 09:17:46 -0700

    save location of database

---
+++ scripts/startMongoServer.py
@@ -0,0 +1,14 @@
+import subprocess
+import socket
+
+#Start Database
+subprocess.call(mongod --dbpath /reg/d/psdm/cxi/cxitut13/res/autosfx/db1 --bind_ip_all)
+
+#Locate GPU Host
+host = socket.gethostname()
+
+#Write Location to File
+filename = "databaseLocation.txt"
+file = open(filename,”w”) 
+file.write("%s"%host) 
+file.close()
--- scripts/startMongoServer.sh
+++
@@ -1,1 +0,0 @@
-mongod --dbpath /reg/d/psdm/cxi/cxitut13/res/autosfx/db1 --bind_ip_all
